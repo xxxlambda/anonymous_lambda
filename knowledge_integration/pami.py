2b9da5a7d439f985a6c1c53b2a00f9e6a67aca55
xxxlambda <zigeguai@gmail.com>
2025-02-26 00:38:11 +0800

    Update pami.py

--- knowledge_integration/pami.py
+++ knowledge_integration/pami.py
@@ -1,11 +1,11 @@
-from knw import knw
+from .knw import knw
 
 
 class pattern_mining(knw):
     def __init__(self):
         super().__init__()
         self.name = "pami"
-        self.description = "The pami library is a Python library for pattern mining. User can choose many algorithms like FP-growth algorithm."
+        self.description = "The pami library is a Python library for pattern mining. User can choose many algorithms like FP-growth algorithm. For Transactional_T10I4D100K.csv, the separator should be '\\t'"
         self.core_function = "pami"
         self.mode = 'full'
 
@@ -15,7 +15,7 @@ class pattern_mining(knw):
         from PAMI.frequentPattern.basic import FPGrowth as alg
         fileURL = "https://u-aizu.ac.jp/~udayrage/datasets/transactionalDatabases/Transactional_T10I4D100K.csv"
         minSup = 300
-        obj = alg.FPGrowth(iFile=fileURL, minSup=minSup, sep='\t') #here is sep='tab'
+        obj = alg.FPGrowth(iFile=fileURL, minSup=minSup, sep='\\t') #Note: You should strictly use separator '\\t'
         # obj.startMine()  #deprecated
         obj.mine()
         obj.save('frequentPatternsAtMinSupCount300.txt')
@@ -27,6 +27,6 @@ class pattern_mining(knw):
         """
 
 
-# if __name__ == '__main__':
-#     pami = pami()
-#     print(pami.get_core_function())
+if __name__ == '__main__':
+    pami = pattern_mining()
+    print(pami.get_core_function())
