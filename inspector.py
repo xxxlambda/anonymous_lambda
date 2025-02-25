d5a34544d943dc8979e364e44e315ef1b42f1fb4
xxxlambda <zigeguai@gmail.com>
2025-02-26 00:34:03 +0800

    Update inspector.py

--- inspector.py
+++ inspector.py
@@ -1,12 +1,13 @@
 import openai
 
-
 class Inspector:
 
-    def __init__(self, api_key , model="gpt-3.5-turbo-1106", base_url=''):
+    def __init__(self, api_key , model="gpt-4o-mini", base_url=''):
         self.client = openai.OpenAI(api_key=api_key, base_url=base_url)
         self.model = model
-        self.messages = []
+        self.messages = [
+
+        ]
         self.function_repository = {}
 
     def add_functions(self, function_lib: dict) -> None:
@@ -26,17 +27,6 @@ class Inspector:
             return self.client.chat.completions.create(**params)
         except Exception as e:
             print(f"Error calling chat model: {e}")
-            return None
-
-    def run(self, function_lib=None):
-        try:
-            if function_lib is None:
-                response = self._call_chat_model()
-                final_response = response.choices[0].message.content
-                return final_response
-
-        except Exception as e:
-            print(f"An error occurred: {e}")
             return None
 
     def clear(self):
